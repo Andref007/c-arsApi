1eb8eeaaa86557ee766d764aa454864b19402d10
Andref007 <andrefps007@hotmail.com>
2022-11-03 21:29:02 -0300

    usando axios

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 const hostname = 'localhost'
 const port = 5000
@@ -11,6 +14,8 @@ app.use(
 )
 
 app.use(express.json())
+
+
 
 const cars = [
     {
